7e442a9a8c7597c3ef68ab01f4bb95a0d4af5859
za5en <79802829+za5en@users.noreply.github.com>
2023-08-11 01:33:16 +0400

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Breakout
+{
+    partial class Breakout
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.hghscore = new System.Windows.Forms.Label();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.specialMessage = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.ForeColor = System.Drawing.Color.White;
+            this.txtScore.Location = new System.Drawing.Point(13, 13);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(81, 24);
+            this.txtScore.TabIndex = 1;
+            this.txtScore.Text = "Счёт: 0";
+            // 
+            // hghscore
+            // 
+            this.hghscore.AutoSize = true;
+            this.hghscore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.hghscore.ForeColor = System.Drawing.Color.White;
+            this.hghscore.Location = new System.Drawing.Point(663, 13);
+            this.hghscore.Name = "hghscore";
+            this.hghscore.Size = new System.Drawing.Size(105, 24);
+            this.hghscore.TabIndex = 3;
+            this.hghscore.Text = "Рекорд: 0";
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.Yellow;
+            this.ball.Location = new System.Drawing.Point(377, 297);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(25, 23);
+            this.ball.TabIndex = 4;
+            this.ball.TabStop = false;
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.White;
+            this.player.Location = new System.Drawing.Point(347, 492);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(100, 32);
+            this.player.TabIndex = 5;
+            this.player.TabStop = false;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.mainGameTimer);
+            // 
+            // specialMessage
+            // 
+            this.specialMessage.AutoSize = true;
+            this.specialMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.specialMessage.ForeColor = System.Drawing.Color.White;
+            this.specialMessage.Location = new System.Drawing.Point(316, 235);
+            this.specialMessage.Name = "specialMessage";
+            this.specialMessage.Size = new System.Drawing.Size(0, 29);
+            this.specialMessage.TabIndex = 6;
+            // 
+            // Breakout
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 536);
+            this.Controls.Add(this.specialMessage);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.hghscore);
+            this.Controls.Add(this.txtScore);
+            this.Name = "Breakout";
+            this.Text = "Breakout";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Label hghscore;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label specialMessage;
+    }
+}
+
